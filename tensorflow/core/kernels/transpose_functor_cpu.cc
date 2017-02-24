7229763e573df5912206e5e7b83333965504e610
luke iwanski <luke@codeplay.com>
2017-02-24 10:55:22 +0000

    Fixes Transpose Op for dobule when on SYCL

--- tensorflow/core/kernels/transpose_functor_cpu.cc
+++ tensorflow/core/kernels/transpose_functor_cpu.cc
@@ -127,9 +127,11 @@ Status DoTranspose<SYCLDevice>(const SYCLDevice& d, const Tensor& in,
   switch (in.dtype()) {
 
     case DT_FLOAT:
-    case DT_DOUBLE:
     case DT_INT32:
       internal::Transpose<SYCLDevice, uint32>(d, in, perm, out);
+      break;
+    case DT_DOUBLE:
+      internal::Transpose<SYCLDevice, uint64>(d, in, perm, out);
       break;
 
     default:
